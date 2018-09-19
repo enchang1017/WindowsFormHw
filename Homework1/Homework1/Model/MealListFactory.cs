7bd362815711bf80a855a7dd03c8c40fdca3a5d2
enchang <enchang1017@gmail.com>
2018-09-19 10:46:59 +0800

    新增菜餚

--- Homework1/Homework1/Model/MealListFactory.cs
+++ Homework1/Homework1/Model/MealListFactory.cs
@@ -9,9 +9,18 @@ namespace Homework1
     public class MealListFactory
     {
         private List<Meal> _mealsList;
+
+        const string MACKERELSUSHIROAST = "烤鯖魚押壽司";
+        const int LOWERPRICE = 40;
+
+        /// <summary>
+        /// 菜單生成
+        /// </summary>
+        /// <returns></returns>
         public List<Meal> CreateMealList()
         {
-            
+            Meal mackerelSushiRoast = new Meal(MACKERELSUSHIROAST, LOWERPRICE);
+
             return _mealsList;
         }
     }
